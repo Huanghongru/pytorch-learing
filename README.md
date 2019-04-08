49a27b5d680726ccedb57f4560d8f65d817ee37f
huanghongru <huanghongru1997@gmail.com>
2019-04-08 17:28:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pytorch learning
+
+learn some pytorch tutorial
---
+++ torch_nn.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+
+        x = x.view(-1, self.num_flat_features(x))
+
+        x = F.relu(self.fc1(x))
+        
+        x = F.relu(self.fc2(x))
+
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+net = Net()
+print net
+
+params = list(net.parameters())
+
+data_in = torch.randn(1,1,32,32)
+data_out = net(data_in)
+
+target = torch.randn(10)
+target = target.view(1,-1)
+criterion = nn.MSELoss()
+
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+optimizer.zero_grad()
+loss = criterion(data_out, target)
+loss.backward()
+optimizer.step()
